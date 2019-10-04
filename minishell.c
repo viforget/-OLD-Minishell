d2082b87bee887308335b43be688eacc841c9ef1
XANA <xana@Tchuuk.local>
2019-10-04 16:14:09 +0200

    Advance in project + emoji

--- minishell.c
+++ minishell.c
@@ -10,33 +10,75 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include "libft/libft.h"
+#include "minishell.h"
 
-int main()
+void	binex(char **av, char *pat)
+{
+	char	 **path;
+	int 	i;
+
+	i = 0;
+	path = ft_strsplit(pat , ':');
+	while(path[i])
+	{
+		path[i] = ft_strjoindel(path[i], "/");
+		execve(ft_strjoin(path[i], av[0]), av, NULL);
+		ft_strdel(&path[i]);
+		i++;
+	}
+	free(path);
+	exit(0);
+}
+
+void	gocd(char *st, char **env)
+{
+	chdir(st);
+	env[7] = ft_strjoindele("PWD=", getcwd(NULL, 4096));
+}
+
+void	goenv(char **env)
+{
+	int i;
+
+	i = 0;
+	while(i < 32)
+	{
+		ft_putendl(env[i]);
+		i++;
+	}
+}
+
+int 	main(int argc, char **argv, char **env)
 {
 	char	*str;
 	char	**av;
 	int i;
 
+	env[7] = ft_strdup(env[7]);
+	env[13] = ft_strdup(env[13]);
 	i = 0;
 	str = ft_memalloc(4097);
 	av = NULL;
 	//execve(ft_strjoindele("/usr/bin", str), NULL, NULL);
-	while (av == NULL || ft_strcmp(av[1], "exit") != 0)
+	while (av == NULL || ft_strcmp(av[0], "exit") != 0)
 	{
-		ft_putstr("$>");
+		putprompt(env);
 		i = read(1, str, 4097);
 		str[i - 1] = 0;
 		av = ft_strsplit(str, ' ');
-		if (/*check_bin(av) != NULL*/ 1)
+		if (ft_strcmp(av[0], "cd") == 0)
+			gocd(av[1], env);
+		else if (ft_strcmp(str, "env") == 0)
+			goenv(env);
+		else if (/*check_bin(av) != NULL*/ 1)
 		{
 			i = fork();
 			if (i == 0)
 			{
-				execve(ft_strjoin("/bin/", av[0]), av, NULL);
+				binex(av, env[13] + 5);
 			}
 			wait(&i);
 		}
 	}
+	ft_strdel(&str);
 }
---
+++ minishell.h
@@ -0,0 +1,9 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "libft/libft.h"
+
+void	putprompt(char **env);
+int		findlastslash(char *str);
+
+#endif
